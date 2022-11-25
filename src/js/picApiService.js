bc15383afced2a4d438949a395f81c78782a7735
Pohorielova <anna.pogorielova@gmail.com>
2022-11-25 17:29:59 +0100

    Update picApiService.js

--- src/js/picApiService.js
+++ src/js/picApiService.js
@@ -4,9 +4,9 @@ export { fetchPictures };
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 const KEY = '31598593-79bd95191575728407a30b47c';
 
-async function fetchPictures(query, page, perPage) {
+async function fetchPictures(query) {
   const response = await axios.get(
-    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
+    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`,
   );
   return response;
 }
