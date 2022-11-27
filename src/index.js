e65a7ff429f3b7a60b7723ca0cac039e9071c6d6
Pohorielova <anna.pogorielova@gmail.com>
2022-11-27 16:41:09 +0100

    +

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './sass/index.scss';
-import { fetchPictures } from './js/picApiService.js';
+// import { fetchPictures } from './js/picApiService.js';
+import PicApiService from './js/picApiService.js';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
@@ -8,42 +9,81 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 const searchForm = document.querySelector('.search-form');
 const galleryBox = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
-let query ='';
-let simpleLightBox ='';
+// ----------вариант 1, не работает
+// let query ='';
+// let simpleLightBox ='';
+// searchForm.addEventListener('submit', onSearch);
+// let page = 1;
+// const perPage = 40;
+// // search pic fn
+// function onSearch (evt){
+//   evt.preventDefault();
+//   page = 1;
+//   query = evt.currentTarget.elements.searchQuery.value.trim();
+//   // galleryBox.innerHTML = '';
+//   loadMoreBtn.classList.add('is-hidden');
+//   if (!query){
+//     Notify.failure('The search string cannot be empty. Please specify your search query.');
+//     return;
+//   }
+//   fetchPictures(query, page, perPage).then(({data}) => {
+//            renderMarkup(data.hits);
+//            simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+//            Notify.success(`Hooray! We found ${data.totalHits} images.`);
+//          })
+//          .catch(error => console.log(error))
+//          .finally(() => {
+//           searchForm.reset();
+//         });
+// };
+// function renderMarkup (data) {
+//   const markup = data.hits.map(image => {
+//     const { id, largeImageURL, webformatURL, tags, likes, views, comments, downloads } = image;
+//     return
+//     `<a class="gallery__item" href="${largeImageURL}">
+//       <div class="photo-card">
+//        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+//          <div class="info">
+//          <p class="info-item"><b>Likes</b>${likes}</p>
+//          <p class="info-item"><b>Views</b>${views}</p>
+//          <p class="info-item"><b>Comments</b>${comments}</p>
+//          <p class="info-item"><b>Downloads</b>${downloads}</p>
+//          </div>
+//       </div>
+//     </a>`;
+//   }).join('');
+//   galleryBox.insertAdjacentHTML('beforeend', markup);
+//     };
+
+
 searchForm.addEventListener('submit', onSearch);
-
-// search pic fn
-function onSearch (evt){
-  evt.preventDefault();
-  query = evt.currentTarget.searchQuery.value.trim();
-  // galleryBox.innerHTML = '';
-  loadMoreBtn.classList.add('is-hidden');
-  if (!query){
-    Notify.failure('The search string cannot be empty. Please specify your search query.');
-    return;
-  }
-
-  fetchPictures(query).then(({data}) => {
-       console.log(data);
-           renderMarkup(data);
-           simpleLightBox = new SimpleLightbox('.gallery a').refresh();
-           Notify.success(`Hooray! We found ${data.totalHits} images.`);
-         })
-         .catch(error => console.log(error))
-         .finally(() => {
-          searchForm.reset();
-        });
-
-};
+// ----------вариант 2, пробуем
+loadMoreBtn.addEventListener('click', onloadMore);
+const picApiService = new PicApiService();
+// let searchQuery ='';
 
 
-function renderMarkup (images) {
-  const markup = images.map(image => {
-    const { id, largeImageURL, webformatURL, tags, likes, views, comments, downloads } = image;
-    return
-    `<a class="gallery__item" href="${largeImageURL}">
+
+
+  function onSearch(e){
+    e.preventDefault();
+    picApiService.query = e.currentTarget.elements.searchQuery.value;
+    picApiService.resetPage();
+    picApiService.fetchPictures().then(createMarkup);
+  };
+
+  function onloadMore(e){
+    picApiService.fetchPictures().then(createMarkup);
+  };
+
+//
+
+function createMarkup(hits) {
+  console.log(`Rendering...`);
+  const markup = hits.map(({ largeImageURL, webformatURL, tags, likes, views, comments, downloads })=>{
+    return `<a class="gallery__item" href="${largeImageURL}">
       <div class="photo-card">
-       <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+       <img src="${webformatURL}" alt="${tags}" width = "400" loading="lazy" />
          <div class="info">
          <p class="info-item"><b>Likes</b>${likes}</p>
          <p class="info-item"><b>Views</b>${views}</p>
@@ -53,7 +93,4 @@ function renderMarkup (images) {
       </div>
     </a>`;
   }).join('');
-
-  galleryBox.insertAdjacentHTML('beforeend', markup);
-    };
-
+  galleryBox.insertAdjacentHTML('beforeend',markup );};
--- src/js/picApiService.js
+++ src/js/picApiService.js
@@ -1,30 +1,65 @@
-import axios from 'axios';
+
+// ----------вариант 1, не работает
+// import axios from 'axios';
 
 
-axios.defaults.baseURL = 'https://pixabay.com/api/';
-const KEY = '31598593-79bd95191575728407a30b47c';
+// axios.defaults.baseURL = 'https://pixabay.com/api/';
+// const KEY = '31598593-79bd95191575728407a30b47c';
 
-// async function fetchPictures(query) {
+// async function fetchPictures(query, page, perPage) {
 //   const response = await axios.get(
-//     `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`,
+//     `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
 //   );
 //   return response;
 // }
 // export { fetchPictures };
-export const fetchPictures = async (query) => {
-  return await fetch(
-    `https://pixabay.com/api/?key=31598593-79bd95191575728407a30b47c&q=${query}&orientation=horizontal&safesearch=true&image_type=photo`
-  )
-    .then(async response => {
-      if (!response.ok) {
-        if (response.status === 404) {
-          return [];
-        }
-        throw new Error(response.status);
-      }
-      return await response.json();
-    })
-    .catch(error => {
-      console.error(error);
-    });
-};
+
+
+// --------вариант 2, не работает
+// export const fetchPictures = async (query, page, perPage) => {
+//   return await fetch(
+//     `https://pixabay.com/api/?key=31598593-79bd95191575728407a30b47c&q=${query}&orientation=horizontal&safesearch=true&image_type=photo&page=${page}&per_page=${perPage}`
+//   )
+//     .then(async response => {
+//       if (!response.ok) {
+//         if (response.status === 404) {
+//           return [];
+//         }
+//         throw new Error(response.status);
+//       }
+//       return await response.json();
+//     })
+//     .catch(error => {
+//       console.error(error);
+//     });
+// };
+export default class PicApiService{
+// ---------------вариант 3, пробуем
+constructor(){
+  this.searchQuery = '';
+  this.page=1;
+}
+
+fetchPictures(){
+
+  return fetch(`https://pixabay.com/api/?key=31598593-79bd95191575728407a30b47c&q=${this.searchQuery}&orientation=horizontal&safesearch=true&image_type=photo&page=${this.page}&per_page=5`)
+  .then( r=> r.json())
+  .then(data=>{
+
+    this.page+=1;
+
+    return data.hits;
+  })
+}
+
+resetPage(){
+  this.page =1;
+}
+get query(){
+  return this.searchQuery;
+}
+
+set query(newQuery){
+  this.searchQuery =newQuery;
+}
+}
